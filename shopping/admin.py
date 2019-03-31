5dc476a2636381922327ca021b2a744e2085c1b8
nic <406722575@qq.com>
2019-04-01 00:10:40 +0800

    注册后台使用装饰器方法

--- shopping/admin.py
+++ shopping/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from .models import ProductCategory,Productlist
 
 # Register your models here.
-
+@admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     list_display =  ['id','名称','描述', '图片']
 
-admin.site.register(ProductCategory,ProductCategoryAdmin)
+# admin.site.register(ProductCategory,ProductCategoryAdmin)
 
-
+@admin.register(Productlist)
 class ProductlistAdmin(admin.ModelAdmin):
     list_display =  ['id','名称','图片', '所属类别','价格','库存','已上架','创建时间','修改时间']
     list_editable = ['名称','所属类别','价格','库存','已上架']
     list_per_page = 2
 
-admin.site.register(Productlist,ProductlistAdmin)
+# admin.site.register(Productlist,ProductlistAdmin)
